b439d2b1781163eda4c80f2715cb1eb932abed6b
bigbib <standardvideos@gmail.com>
2023-12-03 19:59:43 +0100

    03/12/2023

--- livre_recettes/settings.py
+++ livre_recettes/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-enke9-#c4lff-cgjccwuuq6nfa*^6a)(d=+9czaoq3m6mt7mfx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
